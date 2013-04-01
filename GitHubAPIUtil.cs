4e8b10dade383fcf90f381c58829a685128b5923
Jonny Olliff-Lee <jonny.ollifflee@gmail.com>
2013-04-01 23:13:37 +0100

    Added method to enable use of Proxy

--- GitHubAPIUtil.cs
+++ GitHubAPIUtil.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using RestSharp;
+using System.Configuration;
 
 namespace GitHub_Tag_Deployer
 {
@@ -19,10 +21,12 @@ namespace GitHub_Tag_Deployer
             restClient = new RestClient { BaseUrl = "https://github.com" };
         }
 
-        public void fetchTag(string username, string password, string tagUrl, string deploymentDir)
+        public void fetchTag(string username, string password, string tagUrl, string deploymentDir, bool? proxy)
         {
 
             restClient.Authenticator = new HttpBasicAuthenticator(username, password);
+            
+            SetupProxy(proxy);
 
             var restRequest = new RestRequest { Resource = tagUrl.Replace("https://github.com", "") };
 
@@ -32,5 +36,23 @@ namespace GitHub_Tag_Deployer
             fileStream.Write(response.RawBytes, 0, response.RawBytes.Length);
             fileStream.Close();
         }
+
+        private void SetupProxy(bool? useProxy)
+        {
+            if (useProxy == true)
+            {
+                string proxyUserName = ConfigurationManager.AppSettings["ProxyUsername"];
+                string proxyPassword = ConfigurationManager.AppSettings["ProxyPassword"];
+                string proxyDomain = ConfigurationManager.AppSettings["ProxyDomain"];
+                string proxyServer = ConfigurationManager.AppSettings["ProxyServer"];
+                string proxyPort = ConfigurationManager.AppSettings["ProxyPort"];
+
+                ICredentials credentials = new NetworkCredential(proxyUserName, proxyPassword, proxyDomain);
+
+                restClient.Proxy = new WebProxy(proxyServer + ":" + proxyPort, true, new string[0], credentials);
+            }
+        }
+
+        
     }
 }
