8038a1a6cf17600268fd2f96471111b1d9ef517d
Jonny Olliff-Lee <jonny.ollifflee@gmail.com>
2013-04-09 08:47:56 +0100

    Added Model for storing settings

---
+++ Models/ISettingsStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTagDeployer.Models
+{
+    interface ISettingsStorage
+    {
+        string Username { get; set; }
+        string DeployUrl { get; set; }
+        string RollbackUrl { get; set; }
+        string DeployDirectory { get; set; }
+        string BackupDirectory { get; set; }
+        bool DeleteExisting { get; set; }
+        bool UseProxy { get; set; }
+    }
+}
